f75c3385e28b2a7fbd11a17d8053e12fcf2bc0b3
Ryan <ryan-gg@outlook.com>
2025-05-23 00:01:39 +0530

    feat: enhance timesheet functionality with start and end task methods

--- src/main.py
+++ src/main.py
@@ -1,14 +1,49 @@
-from timesheet import CSVTimesheetReader
+import time
+from datetime import datetime
+from typing import Optional
+
+from timesheet import TimesheetReader
 from utils import get_current_timesheet_path
+
+
+def start_task(project: str, description: str, tags: Optional[list[str]] = None):
+    """
+    Start a task with the given project, description, and optional tags.
+    """
+    if tags is None:
+        tags = []
+    print(f"Starting task: {project}, {description}, {tags}")
+    # ROWS = ["Project", "Description", "Tags", "Start DateTime", "End DateTime", "Duration"]
+    timestamp = datetime.fromtimestamp(time.time()).strftime("%Y-%m-%dT%H:%M:%S")
+    rows = [project, description, ", ".join(tags), timestamp, "", ""]
+    timesheet_path = get_current_timesheet_path()
+    reader = TimesheetReader(timesheet_path)
+    reader.append_row(rows)
+
+
+def end_task():
+    """
+    End a task with the given project, description, and optional tags.
+    """
+    timesheet_path = get_current_timesheet_path()
+    reader = TimesheetReader(timesheet_path)
+    row = reader.get_last_row()
+    timestamp = datetime.fromtimestamp(time.time()).strftime("%Y-%m-%dT%H:%M:%S")
+    row[-2] = timestamp
+    row[-1] = str(
+        datetime.strptime(row[-2], "%Y-%m-%dT%H:%M:%S")
+        - datetime.strptime(row[-3], "%Y-%m-%dT%H:%M:%S")
+    )
+    reader.edit_last_row(row)
 
 
 def main():
     timesheet_path = get_current_timesheet_path()
     print(f"Current timesheet path: {timesheet_path}")
 
-    reader = CSVTimesheetReader(timesheet_path)
-    data = reader.read()
-    print(f"Data read from timesheet: {data}")
+    reader = TimesheetReader(timesheet_path)
+    data = reader.get_last_row()
+    print(data)
 
 
 if __name__ == "__main__":
--- src/timesheet.py
+++ src/timesheet.py
@@ -1,32 +1,55 @@
 import csv
-from typing import Any, Dict, List
+from typing import List
+
+ROWS = ["Project", "Description", "Tags", "Start DateTime", "End DateTime", "Duration"]
 
 
-class CSVTimesheetReader:
+class TimesheetReader:
     def __init__(self, path: str):
         """
         Initialize CSV timesheet reader with file path.
         """
         self.path = path
+        self._data = self._read()
+        self.header = self._data[0]
+        self.rows = self._data[1:]
 
-    def read(self) -> List[Dict[str, Any]]:
+    def _read(self) -> List[list[str]]:
         """
-        Read CSV file and return list of dictionaries.
+        Read CSV file and return list of rows.
         """
         with open(self.path, "r", newline="") as file:
-            reader = csv.reader(file, delimiter=",")
-            headers = next(reader)
-            return [dict(zip(headers, row)) for row in reader]
+            reader = csv.reader(file, delimiter=";")
+            return [row for row in reader]
 
-    def write(self, data: List[Dict[str, Any]]) -> None:
+    def _write(self):
         """
-        Write list of dictionaries to CSV file.
+        Write all `data` rows to CSV file, along with `header`.
         """
-        if not data:
-            return
-
-        fieldnames = data[0].keys()
         with open(self.path, "w", newline="") as file:
-            writer = csv.DictWriter(file, fieldnames=fieldnames)
-            writer.writeheader()
-            writer.writerows(data)
+            writer = csv.writer(file, delimiter=";")
+            writer.writerows([self.header])
+            writer.writerows(self._data)
+
+    def append_row(self, row: list[str]):
+        """
+        Append a row to the CSV file.
+        """
+        self._data.append(row)
+        self._write()
+
+    def edit_last_row(self, row: list[str]):
+        """
+        Append a row to the CSV file.
+        """
+        self._data.pop()
+        self._data.append(row)
+        self._write()
+
+    def get_last_row(self) -> list[str]:
+        """
+        Get the last row of the CSV file.
+        """
+        if not self.rows:
+            raise ValueError("No rows found in the CSV file.")
+        return self.rows[-1]
